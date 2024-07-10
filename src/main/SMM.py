c7a0aeb8b0bb691bb2ef53a04e6c9a80b65cbb21
Wenlin <121435520+zipging@users.noreply.github.com>
2024-07-10 15:29:47 +0800

    Update SMM.py

--- src/main/SMM.py
+++ src/main/SMM.py
@@ -21,64 +21,6 @@ def calculate_S_mle_k(X, Omega_ik, x_k):
     S_mle_k = torch.mm(torch.mul(Omega_ik.unsqueeze(dim=0).repeat(D, 1), X_minus_mu.t()), X_minus_mu)
     return S_mle_k
 
-def isPD(B):
-    """Returns true when input is positive-definite, via Cholesky"""
-    try:
-        _ = la.cholesky(B)
-        return True
-    except la.LinAlgError:
-        return False
-def nearestPD(A):
-    """Find the nearest positive-definite matrix to input
-
-    A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
-    credits [2].
-
-    [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
-
-    [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
-    matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
-    """
-    
-
-
-    #H = np.dot(V.T, np.dot(np.diag(s), V))
-    
-    A = torch.tensor(A).to(device)
-    B = (A + A.T) / 2
-    _, s, V = la.svd(B)
-    
-    s = torch.tensor(s).to(device)
-    V = torch.tensor(V).to(device)
-    q = torch.diag(s)*V
-    H = V.T*q
-    #H = V.T@torch.diag(s)@V
-    A2 = (B + H) / 2
-
-    A3 = (A2 + A2.T) / 2
-
-    if isPD(A3):
-        return A3+1e-4*torch.eye(A3.size(0)).to(device)
-    
-    spacing = np.spacing(la.norm(A).cpu())
-    # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
-    # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
-    # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
-    # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-    # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
-    # `spacing` will, for Gaussian random matrixes of small dimension, be on
-    # othe order of 1e-16. In practice, both ways converge, as the unit test
-    # below suggests.
-    I = torch.eye(A.size(0)).to(device)
-    k = 1
-    while not isPD(A3):
-        mineig = torch.min(torch.real(la.eigvals(A3)))
-        A3 += I * (-mineig * k**2 + torch.tensor(spacing).to(device))
-        k += 1
-
-    return A3+1e-4*I
-
 # ok
 def calculate_xi(X, Theta_updated,clusters):  
     K = len(Theta_updated)  # the number of clusters
